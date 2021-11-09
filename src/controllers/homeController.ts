2691a9a7a689b882a6f37f03119195172fe8c9e6
João Vitor <77819780+JoaoVitorJJV@users.noreply.github.com>
2021-11-09 20:58:49 -0300

    Update homeController.ts

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -3,30 +3,5 @@ import {Request, Response} from 'express';
 import { Product } from '../models/Product';
 
 export const index = (req: Request, res: Response)=>{
-    let idade: number = 18;
-    let mensagemIdade: boolean = false;
-    let getProductAfter = false;
-
-    if(idade >= 18){
-        mensagemIdade = true;
-    }
-
-    let list = Product.getAll();
-    let expressionList = Product.getFromPriceAfter(5);
-
-    if(expressionList){
-        getProductAfter = true;
-    }
-
-    res.render('pages/home', {
-        nome : 'João Vitor',
-        mensagemIdade,
-        verificacaoPreco: getProductAfter,
-        expressionList,
-        produtos: list,
-        frases: [
-          ['The day it's beautfuil]
-        ]
-        
-    });
+    res.render('pages/home');
 }
